f92bfc356eadcc164326af0b2128dc1a3c298cef
Ed Phelps <edphelps1@gmail.com>
2018-10-14 11:29:45 -0600

    done

--- serverExpress.js
+++ serverExpress.js
@@ -1,3 +1,11 @@
+// A more complex server that reads / writes file in response to
+// to GET and POST.  Morgan mudule used to console log all requests.
+// Usage:
+// -- http GET localhost:8000/guests
+// -- http POST localhost:8000/guests name=NewPerson
+// -- http GET localhost:8000/guests
+
+
 'use strict';
 
 var fs = require('fs');
@@ -13,6 +21,26 @@ var morgan = require('morgan');
 app.disable('x-powered-by');
 app.use(morgan('short'));
 
+app.use(function(req, res, next) {
+  var bodyJSON = '';
+
+  req.on('data', function(chunk) {
+    bodyJSON += chunk.toString();
+  });
+
+  req.on('end', function() {
+    var body;
+
+    if (bodyJSON !== '') {
+      body = JSON.parse(bodyJSON);
+    }
+
+    req.body = body;
+
+    next();
+  });
+});
+
 app.get('/guests', function(req, res) {
   fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
     if (err) {
@@ -23,6 +51,36 @@ app.get('/guests', function(req, res) {
     var guests = JSON.parse(guestsJSON);
 
     res.send(guests);
+  });
+});
+
+app.post('/guests', function(req, res) {
+  fs.readFile(guestsPath, 'utf8', function(readErr, guestsJSON) {
+    if (readErr) {
+      console.error(readErr.stack);
+      return res.sendStatus(500);
+    }
+
+    var guests = JSON.parse(guestsJSON);
+    var guest = req.body.name;
+
+    if (!guest) {
+      return res.sendStatus(400);
+    }
+
+    guests.push(guest);
+
+    var newGuestsJSON = JSON.stringify(guests);
+
+    fs.writeFile(guestsPath, newGuestsJSON, function(writeErr) {
+      if (writeErr) {
+        console.error(writeErr.stack);
+        return res.sendStatus(500);
+      }
+
+      res.set('Content-Type', 'text/plain');
+      res.send(guest);
+    });
   });
 });
 
